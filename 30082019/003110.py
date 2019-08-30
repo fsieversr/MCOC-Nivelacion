441b44db2ece1c0e1584fdf68fc64bb9e47404ca
Panchi Sievers <fssievers@miuandes.cl>
2019-08-30 00:07:15 -0400

    30082019

---
+++ 30082019/003110.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 29 23:57:40 2019
+
+@author: panch
+"""
+
+#matplotlib
+from matplotlib import pyplot as plt #importa libreria
+
+print(plt.style.available) #muestra los estilos
+
+#plt.style.use('ggplot') #aplica estilo
+plt.xkcd()
+
+
+ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35] #valroes eje x
+
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+
+plt.plot(ages_x, py_dev_y) #grafica en azul y con marcas y ancho de linea 3
+
+js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
+
+plt.plot(ages_x, js_dev_y) #grafica en rojo y ancho de linea 3
+
+dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752] #valores y
+
+plt.plot(ages_x, dev_y, 'k--', marker='.' ) #grafica en negro con rayas y marcas
+
+#nombre eje x
+plt.xlabel("Ages")
+#nombre eje y
+plt.ylabel("Median Salary (USD)")
+#nombre grafico
+plt.title("Median Salary(USD) by Age")
+#leyenda tiene que ir en orden
+plt.legend(["Python", "JavaScript", "All Devs"])
+
+plt.tight_layout() #mejora grafico
+
+#guardar grafico png
+plt.savefig("plot.png")
+
+plt.show() #muestra grafico
+
---
+++ 30082019/003333.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 00:02:09 2019
+
+@author: panch
+"""
+
+#matplotlib
+from matplotlib import pyplot as plt #importa libreria
+
+print(plt.style.available) #muestra los estilos
+
+plt.style.use('ggplot') #aplica estilo
+
+ages_x = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55] #valroes eje x
+
+py_dev_y = [20046, 17100, 20000, 24744, 30500, 37732, 41247, 45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640, 84666,
+            84392, 78254, 85000, 87038, 91991, 100000, 94796, 97962, 93302, 99240, 102736, 112285, 100771, 104708, 108423, 101407, 112542, 122870, 120000]
+
+plt.plot(ages_x, py_dev_y) #grafica en azul y con marcas y ancho de linea 3
+
+js_dev_y = [16446, 16791, 18942, 21780, 25704, 29000, 34372, 37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583, 79000,
+            78508, 79996, 80403, 83820, 88833, 91660, 87892, 96243, 90000, 99313, 91660, 102264, 100000, 100000, 91660, 99240, 108000, 105000, 104000]
+
+plt.plot(ages_x, js_dev_y) #grafica en rojo y ancho de linea 3
+
+dev_y = [17784, 16500, 18012, 20628, 25206, 30252, 34368, 38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752, 77232,
+         78000, 78508, 79536, 82488, 88935, 90000, 90056, 95000, 90000, 91633, 91660, 98150, 98964, 100000, 98988, 100000, 108923, 105000, 103117] #valores y
+
+plt.plot(ages_x, dev_y, 'k--', marker='.' ) #grafica en negro con rayas y marcas
+
+#nombre eje x
+plt.xlabel("Ages")
+#nombre eje y
+plt.ylabel("Median Salary (USD)")
+#nombre grafico
+plt.title("Median Salary(USD) by Age")
+#leyenda tiene que ir en orden
+plt.legend(["Python", "JavaScript", "All Devs"])
+
+plt.tight_layout() #mejora grafico
+
+#guardar grafico png
+plt.savefig("plot1.png")
+
+plt.show() #muestra grafico
